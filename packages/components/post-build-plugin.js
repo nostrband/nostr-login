e454a101bb635241e93cae110c36f57e2f17271e
a-fralou <afralou@yandex.by>
2024-10-21 02:42:26 +0300

    Some fix

--- packages/components/post-build-plugin.js
+++ packages/components/post-build-plugin.js
@@ -3,31 +3,34 @@ const path = require('path');
 
 const folderPath = './dist/components';
 const cssFilePath = './dist/components/css.js';
-let countFileRead = 0
+let countFileRead = 0;
 
+// Function to get all JavaScript files in the directory
 function getFilesInDirectory(dir) {
   const files = fs.readdirSync(dir);
-  return files.filter(file => file.endsWith('.js'));
+  return files.filter(file => file.endsWith('.js')); // Filter out only .js files
 }
 
-// Функция для обработки одного файла
+// Function to process a single file
 function processFile(filePath, cssContent) {
   const fileContent = fs.readFileSync(filePath, 'utf8');
   
+  // Replace the CSS variable declarations and import them from css.js
   const updatedContent = fileContent.replace(/const\s+(\w+Css)\s*=\s*(`[^`]*`|".*?"|'.*?')(?:\s*;|(\s*\/\/.*?))?(?=\s*const|$)/g, (match, varName, varValue) => {
-    // Сохраняем CSS переменные и их значения в cssContent
+    // Save CSS variables and their values in cssContent
     if (countFileRead === 0) {
         cssContent.push(`export const baseCss = ${varValue};`);
         countFileRead = countFileRead + 1;
     }
-    // Возвращаем строку с импортом из css.js
+    // Return the string with the import from css.js
     return `import { baseCss } from './css.js';\nconst ${varName} = baseCss;\n`;
-});
+  });
 
-  // Записываем изменения обратно в файл
+  // Write the modified content back to the file
   fs.writeFileSync(filePath, updatedContent, 'utf8');
 }
 
+// Main function to process all files
 function main() {
   const cssContent = [];
   const files = getFilesInDirectory(folderPath);
@@ -37,14 +40,9 @@ function main() {
     processFile(filePath, cssContent);
   });
 
-  // Записываем собранные CSS переменные в css.js
+  // Write collected CSS variables to css.js
   fs.writeFileSync(cssFilePath, cssContent.join('\n'), 'utf8');
-  console.log('Готово! Все CSS переменные перенесены в css.js.');
+  console.log('Done! All CSS variables have been moved to css.js.');
 }
 
 main();
-
-
-
-
-
